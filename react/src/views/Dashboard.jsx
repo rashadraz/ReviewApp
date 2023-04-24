16b8160c821f771acfcffb377e21e50e792f7027
rashad <akyrashad@gmail.com>
2023-04-24 19:10:35 +0530

    first commit

---
+++ react/src/views/Dashboard.jsx
@@ -0,0 +1,10 @@
+import PageComponent from "../components/PageComponent";
+
+function Dashboard() {
+  return (
+    <>
+      <PageComponent title={"Dashboard"}>Children</PageComponent>
+    </>
+  );
+}
+export default Dashboard;
---
+++ react/src/views/Surveys.jsx
@@ -0,0 +1,10 @@
+import PageComponent from "../components/PageComponent";
+
+function Surveys() {
+  return (
+    <>
+      <PageComponent title={"Surveys"}>Children</PageComponent>
+    </>
+  );
+}
+export default Surveys;
