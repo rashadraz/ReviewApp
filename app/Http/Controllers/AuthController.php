eecbeb60c1118efb345f9b507da21c3750b7d70e
rashad <akyrashad@gmail.com>
2023-04-25 08:02:25 +0530

    login controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+
+    }
+
+    public function login (LoginRequest $request)
+    {
+
+    }
+
+    public function logout ( $request)
+    {
+        
+    }
+}
