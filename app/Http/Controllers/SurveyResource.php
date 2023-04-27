9c9536881131fba352f488206fa88f20f3c56a93
rashad <akyrashad@gmail.com>
2023-04-27 07:56:10 +0530

    survey crud not completed

---
+++ app/Http/Controllers/SurveyResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SurveyStoreRequest;
+use App\Http\Requests\SurveyUpdateRequest;
+use Illuminate\Http\Request;
+
+class SurveyResource extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SurveyStoreRequest $request)
+    {
+        $user = $request->user();
+        return Survey::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(SurveyUpdateRequest $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
